68d44a80cefff6da2b3773172aecad60e1dbfd67
Vikram Singh <vikram@devtron.ai>
2022-04-23 12:28:54 +0530

    test

--- api/restHandler/WebhookEventHandler.go
+++ api/restHandler/WebhookEventHandler.go
@@ -84,6 +84,8 @@ func (impl WebhookEventHandlerImpl) OnWebhookEvent(w http.ResponseWriter, r *htt
 		common.WriteJsonResp(w, err, nil, http.StatusInternalServerError)
 		return
 	}
+	impl.logger.Infow("release info content ..", "bytes", requestBodyBytes)
+	impl.logger.Infow("release info content ..", "string", string(requestBodyBytes))
 
 	isValidSig := impl.webhookSecretValidator.ValidateSecret(r, secretFromRequest, requestBodyBytes, gitHost)
 	impl.logger.Debug("Secret validation result ", isValidSig)
